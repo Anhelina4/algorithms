65d13a1154a692bf358512d0f805ada34873a5b1
Anhelina <bogdanova_angelina@ukr.net>
2022-09-13 10:23:33 +0300

    1

---
+++ index.js
@@ -0,0 +1,24 @@
+import LinkedList from '../algorithms/LinkedList/LinkedList.js'
+import LinkedListNode from '../algorithms/LinkedList/LinkedListNode.js'
+import Queue from '../algorithms/Queue/Queue.js'
+import Stack from '../algorithms/Stack/Stack.js'
+
+// --------------------------------------------LINKED_LIST--------------------------------------------
+// const list = new LinkedListNode(50)
+// const list2 = new LinkedList()
+// list2.prepend(51)
+// list2.prepend(52)
+// console.log('list', list)
+// console.log('list2', list2)
+
+// --------------------------------------------QUEUE--------------------------------------------------
+// const queue = new Queue()
+// queue.enqueue(1)
+// console.log('queue', queue)
+// console.log('queue isEmpty', queue.isEmpty())
+
+// --------------------------------------------STACK--------------------------------------------------
+const stack = new Stack()
+stack.push(1)
+console.log('stack', stack)
+console.log('stack isEmpty', stack.isEmpty())
